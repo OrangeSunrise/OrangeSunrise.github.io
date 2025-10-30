bf34e41e1ad7052379d1f1ab3da5d9578e24ab65
OrangeSunrise <2812506866@qq.com>
2025-10-30 15:56:02 +0800

    二次忽略_site文件夹

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1761809052',resources: [
+    cacheName: 'chirpy-1761810874',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
