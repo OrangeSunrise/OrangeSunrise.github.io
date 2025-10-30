9ac54859fda16bf35e8556549ed93f5171cca720
OrangeSunrise <2812506866@qq.com>
2025-10-30 15:00:42 +0800

    恢复被忽略的_site文件夹

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1761804559',resources: [
+      '/https://OrangeSunrise.github.io/assets/css/jekyll-theme-chirpy.css',
+      '/https://OrangeSunrise.github.io/',
+      
+        '/https://OrangeSunrise.github.io/categories/',
+      
+        '/https://OrangeSunrise.github.io/tags/',
+      
+        '/https://OrangeSunrise.github.io/archives/',
+      
+        '/https://OrangeSunrise.github.io/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
